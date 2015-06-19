4e5e20c58993f9f5649cd4abba860fe5d94aac91
Valera Abornev <dr.webmaker@gmail.com>
2015-06-19 10:43:25 +0300

    added tasks for regexp

--- Error Handing and Regexp/lockedBox.js
+++ Error Handing and Regexp/lockedBox.js
@@ -1,3 +1,8 @@
+/*
+ Consider the following (rather contrived) object:
+*/
+
+
 var box = {
   locked: true,
   unlock: function() { this.locked = false; },
@@ -8,6 +13,18 @@ var box = {
     return this._content;
   }
 };
+
+
+/*
+ It is a box with a lock. Inside is an array, but you can get at it only when the box is unlocked. Directly accessing the
+ _content property is not allowed.
+
+ Write a function called withBoxUnlocked that takes a function value as argument, unlocks the box, runs the function,
+ and then ensures that the box is locked again before returning, regardless of whether the argument function returned
+ normally or threw an exception.
+
+ For extra points, make sure that if you call withBoxUnlocked when the box is already unlocked, the box stays unlocked.
+ */
 
 
 function withBoxUnlocked(body) {
